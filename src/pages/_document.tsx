f8b2cacdd268a1619a4a941451a8b44dbe34ecfa
renepeter <rene.peter18@gmail.com>
2023-04-18 00:32:05 -0400

    add robots, remove unused fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta name="robots" content="index, follow" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
@@ -13,7 +14,7 @@ export default function Document() {
           crossOrigin="anonymous"
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Delicious+Handrawn&family=Inter:wght@400;500;600;700;800;900&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;800&display=swap"
           rel="stylesheet"
         />
       </Head>
