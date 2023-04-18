b89b9413078c25e0e3c9996d954be41b5277132f
renepeter <rene.peter18@gmail.com>
2023-04-18 00:32:59 -0400

    add meta tags

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,14 +2,26 @@ import AboutSection from "@/components/sections/AboutSection";
 import ContactSection from "@/components/sections/ContactSection";
 import IntroSection from "@/components/sections/IntroSection";
 import SkillSection from "@/components/sections/SkillSection";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <article id="home">
-      <IntroSection />
-      <AboutSection />
-      <SkillSection />
-      <ContactSection />
-    </article>
+    <>
+      <Head>
+        <title>Full-Stack Website Engineer - René Peter</title>
+        <meta
+          name="description"
+          content="Building websites, dynamic web applications and integral APIs. Maintaining web infrastructure and solving critical business problems one line of code at a time."
+        />
+        <link rel="canonical" href="https://renepeter.xyz" />
+      </Head>
+
+      <article id="home">
+        <IntroSection />
+        <AboutSection />
+        <SkillSection />
+        <ContactSection />
+      </article>
+    </>
   );
 }
--- src/pages/portfolio/index.tsx
+++ src/pages/portfolio/index.tsx
@@ -1,21 +1,34 @@
+import Head from "next/head";
+
 const posts = Array(10).fill(null);
 
 export default function PortfolioPage() {
   return (
-    <article id="portfolio" className="min-h-screen py-[25vh]">
-      <div className="w-full mx-auto px-6 lg:px-12">
-        <h1 className="font-extrabold text-4xl md:text-5xl lg:text-7xl text-white text-center mb-10 lg:mb-20">
-          Recent Work
-        </h1>
-      </div>
+    <>
+      <Head>
+        <title>Recent Work - René Peter</title>
+        <meta
+          name="description"
+          content="Some of my more recent projects, along with some older stuff that I'm still very proud to say I've worked on."
+        />
+        <link rel="canonical" href="https://renepeter.xyz/portfolio" />
+      </Head>
 
-      <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-px">
-        {posts.map((n, i) => (
-          <li key={i} role="presentation">
-            <div className="w-full h-[300px] bg-gray-950"></div>
-          </li>
-        ))}
-      </ul>
-    </article>
+      <article id="portfolio" className="min-h-screen py-[25vh]">
+        <div className="w-full mx-auto px-6 lg:px-12">
+          <h1 className="font-extrabold text-4xl md:text-5xl lg:text-7xl text-white text-center mb-10 lg:mb-20">
+            Recent Work
+          </h1>
+        </div>
+
+        <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-px">
+          {posts.map((n, i) => (
+            <li key={i} role="presentation">
+              <div className="w-full h-[300px] bg-gray-950"></div>
+            </li>
+          ))}
+        </ul>
+      </article>
+    </>
   );
 }
