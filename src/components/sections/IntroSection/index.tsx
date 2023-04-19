0987d2ceb77235f56e092ef38393bd1617e69578
renepeter <rene.peter18@gmail.com>
2023-04-18 20:42:51 -0400

    update title text styling

--- src/components/sections/IntroSection/index.tsx
+++ src/components/sections/IntroSection/index.tsx
@@ -7,7 +7,7 @@ const IntroSection = () => {
       <div className="w-full mx-auto max-w-screen-xl px-6 lg:px-12">
         <div className="flex gap-10">
           <div className="flex-1">
-            <h1 className="text-4xl md:text-5xl lg:text-7xl font-extrabold tracking-tight text-white leading-tight mb-3 lg:mb-6">
+            <h1 className="text-5xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-[8rem] font-extrabold tracking-tight text-white leading-tight sm:leading-tight md:leading-tight lg:leading-tighter xl:leading-tighter mb-3 lg:mb-6">
               Hi I&apos;m René, <br />a full-stack{" "}
               <span className="gr-teal-limegreen">website engineer</span>.
             </h1>
